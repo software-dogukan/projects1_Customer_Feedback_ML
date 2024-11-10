1e5deddf2cf4e42e3b2a9ef2281a9b8dfbc4f3b5
Dogukan Alkan <78702122+software-dogukan@users.noreply.github.com>
2024-11-10 12:21:50 +0300

    Update Customer_Feedback_ML.py

--- Customer_Feedback_ML.py
+++ Customer_Feedback_ML.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
@@ -7,59 +9,59 @@ customer_data = pd.read_csv("customer_feedback_satisfaction.csv")
 
 # Cinsiyet kolonu: 2. kolonu LabelEncoder ile dönüştürme
 le = preprocessing.LabelEncoder()
-gender = customer_data.iloc[:, 2:3].values  # Cinsiyet kolonu (DataFrame)
-# Cinsiyeti sayısal hale getir
-gender[:, 0] = le.fit_transform(gender[:, 0])
+gender = customer_data.iloc[:, 2].values  # Cinsiyet kolonu (sadece tek boyutlu array)
+
+# Cinsiyet sütununu sayısal hale getir (Male/Female -> 0/1)
+gender_encoded = le.fit_transform(gender)  # Sayısal hale getirme
+
 # OneHotEncoder uygulama (dönüştürülen sayısal cinsiyeti)
 ohe = preprocessing.OneHotEncoder()
-gender = ohe.fit_transform(gender).toarray()  # OneHot kodlamayı 2D diziye çevir
+gender_ohe = ohe.fit_transform(gender_encoded.reshape(-1, 1)).toarray()  # OneHot kodlamayı 2D diziye çevir
 
+# Gender'ı DataFrame'e dönüştürme
+gender_df = pd.DataFrame(gender_ohe, columns=ohe.get_feature_names_out(['Gender']))
 
-country=customer_data.iloc[:,3].values
-country=le.fit_transform(country)
-country=ohe.fit_transform(country.reshape(-1, 1)).toarray()
-unique_words = customer_data['Country'].unique()
-country=pd.DataFrame(country,columns=[unique_words])
-print(country)
+# Ülke (Country) kolonu: LabelEncoder ve OneHotEncoder
+country = customer_data.iloc[:, 3].values
+country_encoded = le.fit_transform(country)  # Sayısal hale getir
+country_ohe = ohe.fit_transform(country_encoded.reshape(-1, 1)).toarray()  # OneHot kodlama
 
-#for feedbackscore
-feedbackscore=customer_data.iloc[:,8].values
-feedbackscore=le.fit_transform(customer_data["FeedbackScore"])
-feedbackscore=ohe.fit_transform(feedbackscore.reshape(-1, 1)).toarray()
-unique_words = le.classes_
-# One-hot encoding sonucu elde edilen feedbackscore verilerini pandas DataFrame'e dönüştürme
-feedbackscore_df = pd.DataFrame(feedbackscore, columns=unique_words)
+# Country'yi DataFrame'e dönüştürme
+unique_country = ohe.get_feature_names_out(['Country'])
+country_df = pd.DataFrame(country_ohe, columns=unique_country)
 
+# FeedbackScore kolonu: LabelEncoder ve OneHotEncoder
+feedbackscore = customer_data.iloc[:, 8].values
+feedbackscore_encoded = le.fit_transform(feedbackscore)  # Sayısal hale getir
+feedbackscore_ohe = ohe.fit_transform(feedbackscore_encoded.reshape(-1, 1)).toarray()  # OneHot kodlama
 
-loyaltyLevel=customer_data.iloc[:,9].values
-loyaltyLevel=le.fit_transform(customer_data["LoyaltyLevel"])
-loyaltyLevel=ohe.fit_transform(loyaltyLevel.reshape(-1, 1)).toarray()
-unique_words = le.classes_
-# One-hot encoding sonucu elde edilen feedbackscore verilerini pandas DataFrame'e dönüştürme
-loyaltyLevel_df = pd.DataFrame(loyaltyLevel, columns=unique_words)
-print(loyaltyLevel_df)
+# FeedbackScore'u DataFrame'e dönüştürme
+unique_feedbackscore = ohe.get_feature_names_out(['FeedbackScore'])
+feedbackscore_df = pd.DataFrame(feedbackscore_ohe, columns=unique_feedbackscore)
 
+# LoyaltyLevel kolonu: LabelEncoder ve OneHotEncoder
+loyaltyLevel = customer_data.iloc[:, 9].values
+loyaltyLevel_encoded = le.fit_transform(loyaltyLevel)  # Sayısal hale getir
+loyaltyLevel_ohe = ohe.fit_transform(loyaltyLevel_encoded.reshape(-1, 1)).toarray()  # OneHot kodlama
 
+# LoyaltyLevel'i DataFrame'e dönüştürme
+unique_loyaltyLevel = ohe.get_feature_names_out(['LoyaltyLevel'])
+loyaltyLevel_df = pd.DataFrame(loyaltyLevel_ohe, columns=unique_loyaltyLevel)
 
 # İlk iki kolonu al
 data1 = customer_data.iloc[:, :2].values
 # Diğer kolonları al
 data2 = customer_data.iloc[:, 4:8].values
-data3=customer_data.iloc[:,-1].values
+data3 = customer_data.iloc[:, -1].values
+
 # numpy array'lerini pandas DataFrame'e dönüştür
-
 data1_df = pd.DataFrame(data1, columns=customer_data.columns[:2])
-gender_df = pd.DataFrame(gender, columns=ohe.get_feature_names_out([customer_data.columns[2]]))
-data2_df = pd.DataFrame(data2, columns=customer_data.columns[3:8])
-data3_df = pd.DataFrame(data3, columns=customer_data.columns[-1])
+data2_df = pd.DataFrame(data2, columns=customer_data.columns[4:8])
+data3_df = pd.DataFrame(data3, columns=[customer_data.columns[-1]])
+
 # DataFrame'leri birleştirme
-new_data1 = pd.concat([data1_df, gender_df.iloc[:,0],country], axis=1)
-new_data2=pd.concat([new_data1,data2_df,feedbackscore_df,loyaltyLevel_df,data3_df],axis=1)
+new_data1 = pd.concat([data1_df, gender_df, country_df], axis=1)
+new_data2 = pd.concat([new_data1, data2_df, feedbackscore_df, loyaltyLevel_df, data3_df], axis=1)
+
+# Sonuçları yazdırma
 print(new_data2)
-
-
-
-
-
-
-
